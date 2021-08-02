b449ec65fef1cd53a1ef65b1a06220b698706b40
Nick Bensema <nickbensema@hotmail.com>
2021-08-01 22:57:54 -0700

    got READY prompt to appear.
    there's the same bytes free and everything.  The cursor doesn't
    blink and the VBI isn't happening at all, but I can use the monitor
    to manually poke characters into the keyboard buffer and run
    simple statements and programs.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dasm roms.a65 -oroms -f3 -lroms.lst -sroms.sym
+result=$?
+if [[ result -ne 0 ]]
+then
+	echo "stopping because dasm returned $result" >/dev/stderr
+	exit $result
+fi
+
+# 8k rom containing most of basic
+dd bs=1024 count=8 <roms >rom.a000
+# 10k rom containing chargen and kernal
+dd bs=1024 skip=14 <roms >rom.d800
+
--- split64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-dd bs=1024 count=8 <roms >rom.a000
-dd bs=1024 skip=16 <roms >rom.e000
-
